58d9492270baca2fc32b0ccedba601f73ddcf461
Leafar <ruizleafar7@gmail.com>
2023-02-08 09:31:20 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'appEjercicios',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
