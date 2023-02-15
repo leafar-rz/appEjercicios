608e61a7c39282f37d9fd75f74b86a6d6d9c16f8
Leafar <ruizleafar7@gmail.com>
2023-02-15 07:42:00 -0600

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,40 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
+  // asi se carga la ruta de redireccionamiento
   {
     path: '',
     redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+   // asi se carga la ruta de redireccionamiento
+  {
+    path: 'login',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'registro',
+    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
+  },
+   // asi se carga la ruta de redireccionamiento
+  {
+    path: 'registro',
+    redirectTo: 'registro',
+    pathMatch: 'full'
+  },
+  {
+    path: 'forgot-password',
+    loadChildren: () => import('./forgot-password/forgot-password.module').then( m => m.ForgotPasswordPageModule)
+  },
+   // asi se carga la ruta de redireccionamiento
+  {
+    path: 'forgot-password',
+    redirectTo: 'forgot-password',
     pathMatch: 'full'
   },
 ];
@@ -18,5 +49,6 @@ const routes: Routes = [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
+
 })
 export class AppRoutingModule { }
